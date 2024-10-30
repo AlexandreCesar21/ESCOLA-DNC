d91f0ea845fa4686f75c669ae47c0af27c88feec
Alexandre César <127257670+AlexandreCesar21@users.noreply.github.com>
2024-10-29 21:22:44 -0300

    Função Javascript

---
+++ script.js
@@ -0,0 +1,21 @@
+var leonardo = document.getElementById('leonardo')
+var samantha = document.getAnimations('samantha')
+var bruna = document.getElementById('bruna')
+
+var seta = document.getElementById('seta-esquerda')
+
+bruna.style = "display:none"
+
+function RolarParaDireita(){
+    leonardo.style = "display:none"
+    bruna.style = "display:flex"
+}
+
+seta.style = "display:flex"
+
+function RolarParaEsquerda(){
+    
+    bruna.style = "display:none"
+    leonardo.style = "display:flex"
+}
+
